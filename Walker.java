c586f150a917ce00fa2014b6aec255c56a33d1f6
damis12 <45795849+damis12@users.noreply.github.com>
2021-06-21 16:06:32 +0200

    Add files via upload
    Battlefield 1.0

---
+++ Walker.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package battlefield_inside;
+
+/**
+ * @author giuseppe damis
+ *
+ */
+public class Walker {
+
+	private Position posizione;
+	private int longevita;
+	
+	public Walker(Position p) {
+		this.posizione = p;
+		this.longevita = 0 ;
+	}
+
+	public Position getPosizione() {
+		return this.posizione;
+	}
+	
+	public int incrementaLongevita() {
+		return ++this.longevita;
+	}
+	
+	public int getLongevita() {
+		return this.longevita;
+	}
+	
+	public void passo(Battlefield field) {
+		Position nuova = this.decidiDoveAndare(field);
+		if (nuova!=null) {
+			Walker clone = new Walker(nuova);
+			field.addWalker(clone);
+		}
+		this.incrementaLongevita();
+	}
+	
+	public Position decidiDoveAndare(Battlefield field) {
+		Position corrente = this.getPosizione();
+		Position libera = field.posizioneLiberaVicino(corrente);
+		return libera; // verso una posizione libera
+					   // tutto occupato: fermo
+	}
+
+}
